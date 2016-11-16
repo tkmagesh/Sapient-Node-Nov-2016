e05149bb98786ab91b97c5a7ae8fe6318fe3e988
Magesh <tkmagesh77@gmail.com>
2016-11-16 14:49:27 +0530

    Calculator Server - [Post]

--- Day-02/10-CalculatorServer/server.js
+++ Day-02/10-CalculatorServer/server.js
@@ -24,7 +24,7 @@ var server = http.createServer(function(req, res){
 				res.end();
 			}
 		});
-	} else if (urlObj.pathname === '/calculator'){
+	} else if (urlObj.pathname === '/calculator' && req.method === 'GET'){
 		var calcData = querystring.parse(urlObj.query);
 		var operation = calcData.op,
 			arg1 = parseInt(calcData.n1, 10),
@@ -34,6 +34,23 @@ var server = http.createServer(function(req, res){
 		res.write(result.toString());
 		res.end();
 		//calculator operation requested
+	} else if (urlObj.pathname === '/calculator' && req.method === 'POST'){
+		var rawData = '';
+		req.setEncoding('utf8');
+		req.on('data', function(chunk){
+			rawData += chunk
+		});
+		req.on('end', function(){
+			var calcData = querystring.parse(rawData);
+			var operation = calcData.op,
+				arg1 = parseInt(calcData.n1, 10),
+				arg2 = parseInt(calcData.n2, 10);
+
+			var result = calculator[operation](arg1, arg2);
+			res.write(result.toString());
+			res.end();
+			
+		})
 	} else {
 		res.statusCode = 404;
 		res.end();
