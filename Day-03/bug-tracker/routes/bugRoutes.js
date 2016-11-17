185e4969849ca146a9c7fe6d261157b0278758a5
Magesh <tkmagesh77@gmail.com>
2016-11-17 15:31:37 +0530

    All use cases of bug tracker

--- Day-03/bug-tracker/routes/bugRoutes.js
+++ Day-03/bug-tracker/routes/bugRoutes.js
@@ -2,9 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 var bugsList = [
-	"Server communication failure", 
-	"user actions not recognized", 
-	"object referenence not set to an object"
+	{id : 1, name : "Server communication failure",  isClosed : false},
+	{id : 2, name : "user actions not recognized",  isClosed : true},
+	{id : 3, name : "object referenence not set to an object", isClosed : false},
 ];
 
 /* GET bugs listing. */
@@ -13,4 +13,55 @@ router.get('/', function(req, res, next) {
   	res.render('bugs/list', viewModel);
 });
 
+router.get('/new', function(req, res, next){
+	res.render('bugs/new');
+});
+
+router.post('/new', function(req, res, next){
+	var bugName = req.body.bugName;
+	var newBug = {
+		id : bugsList.length,
+		name : bugName,
+		isClosed : false
+	};
+	bugsList.push(newBug);
+	res.redirect('/bugs');
+});
+
+router.post('/toggle/:id', function(req, res, next){
+	var bugId = req.params.id;
+	for(var i=0; i < bugsList.length; i++){
+		if (bugsList[i].id == bugId){
+			bugsList[i].isClosed = !bugsList[i].isClosed;
+			res.redirect('/bugs');
+		}
+	}
+});
+
+router.post('/removeClosed', function(req, res, next){
+	for(var i=bugsList.length-1; i >=0 ; i--){
+		if (bugsList[i].isClosed)
+			bugsList.splice(i,1);
+	}
+	res.redirect('/bugs');
+});
+
 module.exports = router;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
